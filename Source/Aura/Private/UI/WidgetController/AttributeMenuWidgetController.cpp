68ed22829df046dfb7a257238d0850cf6a4e0f67
thepinnacle317 <nick.bono0317@gmail.com>
2023-06-30 08:32:55 -0700

    Added DataAsset for the attributes menu widget information

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,18 @@
+// Copyright Pinnacle Gaming Studios
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	Super::BroadcastInitialValues();
+
+	
+}
