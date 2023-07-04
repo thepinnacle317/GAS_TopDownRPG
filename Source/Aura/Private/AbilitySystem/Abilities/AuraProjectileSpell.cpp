9583f85b10f44179dbf4a0566d747a3c7b1e0177
thepinnacle317 <nick.bono0317@gmail.com>
2023-07-04 01:47:46 -0700

    Added spell projectile C++ class and created the firebolt ability

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+// Copyright Pinnacle Gaming Studios
+
+
+#include "AbilitySystem/Abilities/AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	UKismetSystemLibrary::PrintString(this, FString("Activate Ability"), true, true, FLinearColor::Blue, 3);
+}
