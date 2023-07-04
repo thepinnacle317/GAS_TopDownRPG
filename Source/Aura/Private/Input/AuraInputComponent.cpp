21affb0391cd2d5f01f278c3a839babbbd8e747b
thepinnacle317 <nick.bono0317@gmail.com>
2023-07-03 21:32:27 -0700

    Added a DA to the player controller and custom Enhanced Input Component that is input to tag driven for the ability to be activated

---
+++ Source/Aura/Private/Input/AuraInputComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Pinnacle Gaming Studios
+
+
+#include "Input/AuraInputComponent.h"
+
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -4,8 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "GameplayTagContainer.h"
 #include "AuraPlayerController.generated.h"
 
+class UAuraInputConfig;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
@@ -38,4 +40,11 @@ private:
 	void CursorTrace();
 	IEnemyInterface* LastActor;
 	IEnemyInterface* ThisActor;
+
+	void AbilityInputTagPressed(FGameplayTag InputTag);
+	void AbilityInputTagReleased(FGameplayTag InputTag);
+	void AbilityInputTagHeld(FGameplayTag InputTag);
+
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	TObjectPtr<UAuraInputConfig> InputConfig;
 };
